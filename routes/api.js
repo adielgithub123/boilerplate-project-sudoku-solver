6c17dc6b3fce1ae6277556957e3a1f6acfdadfec
adielwd <adielpiero123@gmail.com>
2023-04-18 22:27:14 +0700

    first

--- routes/api.js
+++ routes/api.js
@@ -8,7 +8,34 @@ module.exports = function (app) {
 
   app.route('/api/check')
     .post((req, res) => {
-
+      const puzzle = req.body.puzzle || "";
+      const coordinate = req.body.coordinate || "";
+      const value = req.body.value || "";
+      if(puzzle=="" || coordinate=="" || value==""){
+        res.send({
+          "error": "Required field(s) missing"
+        })
+      }
+      else{
+        const isValidated = solver.validate(puzzle);
+        if(isValidated!==true){
+          res.send(isValidated)
+        }
+        else{
+          const coor = coordinate.split('');
+          if(coor.length!=2){
+            res.send({
+              "error": "Invalid coordinate"
+            })
+          }
+          else{
+            const isValidRowValue = solver.checkRowPlacement(puzzleString,coor[0],coor[1],value);
+            const isValidColValue = solver.checkColPlacement(puzzleString,coor[0],coor[1],value);
+            const isValidRegionValue = solver.checkRegionPlacement(puzzleString,coor[0],coor[1],value);
+            // if(isVal)
+          }
+        }
+      }
     });
     
   app.route('/api/solve')
