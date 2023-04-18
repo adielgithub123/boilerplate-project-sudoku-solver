80dc018352af0657f5b13f5f6e620187ada0c945
adielwd <adielpiero123@gmail.com>
2023-04-18 22:27:51 +0700

    second

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -1,6 +1,21 @@
 class SudokuSolver {
 
   validate(puzzleString) {
+    const regex = /^[1-9\.]{81}$/;
+
+    if (puzzleString.length !== 81) {
+      return {
+        "error": "Expected puzzle to be 81 characters long"
+      };
+    }
+    else if(regex.test(puzzleString)===false){
+      return {
+        "error": "Invalid characters in puzzle"
+      };
+    }
+    else{
+      return true;
+    }
   }
 
   checkRowPlacement(puzzleString, row, column, value) {
